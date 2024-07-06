42a4ae1d80d7e188dd7ebe87bcd99c7a565db97a
Gluon <andrainat@gmail.com>
2024-07-06 13:38:20 +0200

    Method private exclusion doesn't works

--- apibinanceinteraction/src/main/java/gluon/projects/utilities/PropertiesGetter.java
+++ apibinanceinteraction/src/main/java/gluon/projects/utilities/PropertiesGetter.java
@@ -2,14 +2,12 @@ package gluon.projects.utilities;
 
 import gluon.projects.myexception.PropertieFileException;
 
-import javax.annotation.processing.Generated;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesGetter {
 
-    @Generated("jacoco exclude")
     private PropertiesGetter() {
         throw new IllegalStateException("Utility class");
     }
