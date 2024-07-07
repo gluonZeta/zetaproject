1f6434b780dd4ce1fd384d7231a20c8b2c8451b1
Gluon <andrainat@gmail.com>
2024-07-07 16:24:10 +0200

    Add main in the service layer

--- servicemetier/src/main/java/gluon/projects/Main.java
+++ servicemetier/src/main/java/gluon/projects/Main.java
@@ -3,10 +3,8 @@ package gluon.projects;
 import gluon.projects.socketbinance.BinanceWebsocket;
 
 public class Main {
-
     public static void main(String[] args) {
         BinanceWebsocket binanceWebsocket = new BinanceWebsocket();
         binanceWebsocket.websocketConnect();
     }
-
 }
